2ba46e39d0c5259a5bc2c5e21a141b7ac05723f3
Bohdan Cryptolions <37701692+ansigroup@users.noreply.github.com>
2018-06-10 22:13:03 +0300

    Create stop.sh

---
+++ stop.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+################################################################################
+#
+# Scrip Created by http://CryptoLions.io
+# https://github.com/CryptoLions/EOS-Jungle-Testnet
+#
+###############################################################################
+
+DIR="/opt/EOSmainNet"
+
+
+ if [ -f $DIR"/nodeos.pid" ]; then
+	pid=`cat $DIR"/nodeos.pid"`
+	echo $pid
+	kill $pid
+	rm -r $DIR"/nodeos.pid"
+
+	echo -ne "Stoping Nodeos"
+
+        while true; do
+            [ ! -d "/proc/$pid/fd" ] && break
+            echo -ne "."
+            sleep 1
+        done
+        echo -ne "\rNodeos Stopped.    \n"
+    fi
