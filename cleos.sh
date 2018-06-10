479f2916aa8698237eb3a7de8aeccbb90fd8485c
Bohdan Cryptolions <37701692+ansigroup@users.noreply.github.com>
2018-06-10 22:10:50 +0300

    Create cleos.sh

---
+++ cleos.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+################################################################################
+#
+# Scrip Created by http://CryptoLions.io
+# https://github.com/CryptoLions/scripts/
+#
+###############################################################################
+
+NODEOSBINDIR="/home/eos-sources/eos/build/programs"
+
+WALLETHOST="127.0.0.1"
+NODEHOST="127.0.0.1"
+NODEPORT="8888"
+WALLETPORT="3000"
+
+
+
+$NODEOSBINDIR/cleos/cleos -u http://$NODEHOST:$NODEPORT --wallet-url http://$WALLETHOST:$WALLETPORT "$@"
