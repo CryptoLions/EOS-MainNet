ff94e62dc6965415f7e8ab4328df5cf00e8d178d
Bohdan Cryptolions <37701692+ansigroup@users.noreply.github.com>
2018-06-10 22:08:35 +0300

    Create start_wallet.sh

---
+++ Wallet/start_wallet.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+################################################################################
+#
+# Scrip Created by http://CryptoLions.io
+# https://github.com/CryptoLions/EOS-Jungle-Testnet/
+#
+###############################################################################
+
+NODEOSBINDIR="/home/eos-sources/eos/build/programs"
+DATADIR="/opt/JungleTestnet/Wallet"
+WALLET_HOST="127.0.0.1"
+WALLET_POSRT="3000"
+
+
+$DATADIR/stop_wallet.sh
+$NODEOSBINDIR/keosd/keosd --config-dir $DATADIR --wallet-dir $DATADIR --http-server-address $WALLET_HOST:$WALLET_POSRT $@ & echo $! > $DATADIR/wallet.pid
