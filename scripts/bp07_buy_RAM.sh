ae075d575504b6e2e35d4dab9cd58f20c951df4b
Bohdan Cryptolions <37701692+ansigroup@users.noreply.github.com>
2018-06-11 08:29:41 +0300

    Create bp07_buy_RAM.sh

---
+++ scripts/bp07_buy_RAM.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+################################################################################
+#
+# Scrip Created by http://CryptoLions.io
+# For EOS mainnet
+#
+# https://github.com/CryptoLions/EOS-MainNet
+#
+################################################################################
+
+# PAYER ACCOUNT NAME
+PAYER=""
+
+# RECEIVER ACCOUNT NAME
+RECEIVER=""
+
+# AMOUNT IN EOS YOU WILL SPENT TO BUY RAM
+QUANT="0.1000 EOS"
+
+./cleos.sh system buyram $PAYER $RECEIVER "$QUANT" -p $PAYER
