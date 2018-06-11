f69881a3d8b9a3ef0d7e32e5d29a37fd09c1a3ae
Bohdan Cryptolions <37701692+ansigroup@users.noreply.github.com>
2018-06-11 08:09:04 +0300

    Update bp01_registerProducer.sh

--- scripts/bp01_registerProducer.sh
+++ scripts/bp01_registerProducer.sh
@@ -7,9 +7,15 @@
 # https://github.com/CryptoLions/EOS-MainNet
 #
 ################################################################################
-ACCOUNT="PRODUCER_ACCOUNT"
-PUBKEY="PRODUCER_PUB_BLOCK_SIGN_KEY"
-URL="PRODUCER_YRL"
+#PRODUCER_ACCOUNT
+ACCOUNT=""
+
+#PRODUCER_PUB_BLOCK_SIGN_KEY
+PUBKEY=""
+
+#PRODUCER WEB-Page. Please uplad to that addres bp.json for voting portals. Info: https://github.com/eosrio/bp-info-standard
+#URL="http://cryptolions.io"
+URL=""
 
 ./cleos.sh system regproducer $ACCOUNT $PUBKEY "$URL" -p $ACCOUNT
 
