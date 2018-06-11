751bb27abc77688f53daa8cdaf5183663fc55e02
Bohdan Cryptolions <37701692+ansigroup@users.noreply.github.com>
2018-06-11 08:06:57 +0300

    Create bp00_createAccount.sh

---
+++ scripts/bp00_createAccount.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+################################################################################
+#
+# Scrip Created by http://CryptoLions.io
+# For EOS mainnet
+#
+# https://github.com/CryptoLions/
+#
+################################################################################
+
+#YOUR_ACCOUNT_NAME
+CREATOR=""
+
+#NEW_ACCOUNT_NAME
+NEWACC=""
+
+#NEW_ACCOUNT_OWNER_PUB_KEY
+KEY_OWNER_1=""
+
+#NEW_ACCOUNT_ACTIVE_PUB_KEY
+KEY_ACTIVE_1=""
+
+#STAKE to CPU
+STAKE_CPU="1.0000 EOS"
+
+#STAKE to NET
+STAKE_NET="1.0000 EOS"
+
+#Buy RAM kbyts
+BUYRAM=8
+
+./cleos.sh system newaccount --stake-net "$STAKE_CPU" --stake-cpu "$STAKE_NET" --buy-ram-kbytes $BUYRAM $CREATOR $NEWACC $KEY_OWNER_1 $KEY_OWNER_2
+
+
