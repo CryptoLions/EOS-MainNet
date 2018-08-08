cf8a5bff1425404771d6d1895a4dcbe53098984f
Bohdan Cryptolions <37701692+ansigroup@users.noreply.github.com>
2018-08-08 11:39:32 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -14,5 +14,9 @@ NODEOSBINDIR="/home/eos-sources/eos/build/programs"
 
 $DATADIR/stop.sh
 echo -e "Starting Nodeos \n";
+
+ulimit -c unlimited
+ulimit -n 65535
 ulimit -s 64000
+
 $NODEOSBINDIR/nodeos/nodeos --data-dir $DATADIR --config-dir $DATADIR "$@" > $DATADIR/stdout.txt 2> $DATADIR/stderr.txt &  echo $! > $DATADIR/nodeos.pid
