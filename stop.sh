2c01721675a7212b4abd688adc93ce3961c2ed12
Bohdan Cryptolions <37701692+ansigroup@users.noreply.github.com>
2018-10-27 11:50:51 +0300

    Update stop.sh

--- stop.sh
+++ stop.sh
@@ -24,5 +24,12 @@ DIR="/opt/EOSmainNet"
             echo -ne "."
             sleep 1
         done
+	
+	DATE=date -d "now" +'%Y_%m_%d-%H_%M'
+	if [[ ! -d $DIR/logs ]]; then
+    		mkdir $DIR/logs
+	fi
+	tar -pcvzf $DIR/stderr-$DATE.txt.tar.gz stderr.txt stdout.txt
+
         echo -ne "\rNodeos Stopped.    \n"
     fi
