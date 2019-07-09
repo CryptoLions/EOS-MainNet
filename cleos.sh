450e3d27db1179252674e2beca653dab24744384
ohJohanZ <telnetor@163.com>
2019-07-09 18:22:48 +0800

    Update cleos.sh
    We don't require `--wallet-url` any more in new EOSIO version.

--- cleos.sh
+++ cleos.sh
@@ -13,8 +13,4 @@ NODEOSBINDIR="/home/eos-sources/eos/build/programs"
 NODEHOST="127.0.0.1"
 NODEPORT="8888"
 
-WALLETHOST="127.0.0.1"
-WALLETPORT="3000"
-
-
-$NODEOSBINDIR/cleos/cleos -u http://$NODEHOST:$NODEPORT --wallet-url http://$WALLETHOST:$WALLETPORT "$@"
+$NODEOSBINDIR/cleos/cleos -u http://$NODEHOST:$NODEPORT "$@"
